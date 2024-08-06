7c1494c02346b553e61f7b2ed8aa0928fe8d1148
shahobiddin2oo9 <wahobiddin2oo9@gmail.com>
2024-08-06 12:57:43 +0500

    shaha

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(".carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  // nav: true,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    300: {
+      items: 1,
+    },
+    600: {
+      items: 1.2,
+    },
+    700: {
+      items: 1.4,
+    },
+    1000: {
+      items: 3,
+    },
+    1250: {
+      items: 4,
+    },
+    1750: {
+      items: 4,
+    },
+  },
+});
+$(".carousel-1").owlCarousel({
+  loop: true,
+  margin: 10,
+  // nav: true,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    300: {
+      items: 1,
+    },
+    600: {
+      items: 1.2,
+    },
+    700: {
+      items: 1.4,
+    },
+    1000: {
+      items: 3,
+    },
+    1250: {
+      items: 3,
+    },
+    1750: {
+      items: 3,
+    },
+  },
+});
